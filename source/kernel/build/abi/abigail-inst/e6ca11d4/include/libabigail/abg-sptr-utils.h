40e674c238388b3751b6a054e2288d6dfb76b0ff
pswbuild <pswbuild@oppo.com>
2026-02-11 18:51:00 +0800

    Synchronize code for OPPO CPH1989_11.F.27

---
+++ source/kernel/build/abi/abigail-inst/e6ca11d4/include/libabigail/abg-sptr-utils.h
@@ -0,0 +1,143 @@
+// -*- mode: C++ -*-
+//
+// Copyright (C) 2013-2020 Red Hat, Inc.
+//
+// This file is part of the GNU Application Binary Interface Generic
+// Analysis and Instrumentation Library (libabigail).  This library is
+// free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
+// later version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
+
+/// @file
+///
+/// Utilities to ease the wrapping of C types into std::tr1::shared_ptr
+
+#ifndef __ABG_SPTR_UTILS_H__
+#define __ABG_SPTR_UTILS_H__
+
+#include <regex.h>
+#include <libxml/xmlreader.h>
+#include "abg-cxx-compat.h"
+
+namespace abigail
+{
+
+/// Namespace for the utilities to wrap C types into std::tr1::shared_ptr.
+namespace sptr_utils
+{
+
+using abg_compat::shared_ptr;
+
+/// This is to be specialized for the diverse C types that needs
+/// wrapping in shared_ptr.
+///
+/// @tparam T the type of the C type to wrap in a shared_ptr.
+///
+/// @param p a pointer to wrap in a shared_ptr.
+///
+/// @return then newly created shared_ptr<T>
+template<class T>
+shared_ptr<T>
+build_sptr(T* p);
+
+/// This is to be specialized for the diverse C types that needs
+/// wrapping in shared_ptr.
+///
+/// This variant creates a pointer to T and wraps it into a
+/// shared_ptr<T>.
+///
+/// @tparam T the type of the C type to wrap in a shared_ptr.
+///
+/// @return then newly created shared_ptr<T>
+template<class T>
+shared_ptr<T>
+build_sptr();
+
+/// A convenience typedef for a shared pointer of xmlTextReader.
+typedef shared_ptr<xmlTextReader> reader_sptr;
+
+/// Specialization of sptr_utils::build_sptr for xmlTextReader
+template<>
+reader_sptr
+build_sptr<xmlTextReader>(xmlTextReader *p);
+
+/// A convenience typedef for a shared pointer of xmlChar.
+typedef shared_ptr<xmlChar> xml_char_sptr;
+
+/// Specialization of build_str for xmlChar.
+template<>
+xml_char_sptr build_sptr<xmlChar>(xmlChar *p);
+
+/// A convenience typedef for a shared pointer of regex_t.
+typedef shared_ptr<regex_t> regex_t_sptr;
+
+/// Specialization of sptr_utils::build_sptr for regex_t.
+template<>
+regex_t_sptr
+build_sptr<regex_t>(regex_t *p);
+
+/// Specialization of sptr_utils::build_sptr for regex_t.
+template<>
+regex_t_sptr
+build_sptr<regex_t>();
+
+/// A deleter for shared pointers that ... doesn't delete the object
+/// managed by the shared pointer.
+struct noop_deleter
+{
+  template<typename T>
+  void
+  operator()(const T*)
+  {}
+};
+
+/// A delete functor for a shared_ptr of regex_t.
+struct regex_t_deleter
+{
+  /// The operator called to de-allocate the pointer to regex_t
+  /// embedded in a shared_ptr<regex_t>
+  ///
+  /// @param r the pointer to regex_t to de-allocate.
+  void
+  operator()(::regex_t* r)
+  {
+    regfree(r);
+    delete r;
+  }
+};//end struct regex_deleter
+
+/// Specialization of sptr_utils::build_sptr for regex_t.
+///
+/// This is used to wrap a pointer to regex_t into a
+/// shared_ptr<regex_t>.
+///
+/// @param p the bare pointer to regex_t to wrap into a shared_ptr<regex_t>.
+///
+/// @return the shared_ptr<regex_t> that wraps @p p.
+template<>
+regex_t_sptr
+build_sptr<regex_t>(regex_t *p);
+
+/// Specialization of sptr_utils::build_sptr for regex_t.
+///
+/// This creates a pointer to regex_t and wraps it into a shared_ptr<regex_t>.
+///
+/// @return the shared_ptr<regex_t> wrapping the newly created regex_t*
+template<>
+regex_t_sptr
+build_sptr<regex_t>();
+
+}// end namespace sptr_utils
+}// end namespace abigail
+
+#endif //__ABG_SPTR_UTILS_H__
---
+++ source/kernel/build/abi/abigail-inst/e6ca11d4/include/libabigail/abg-writer.h
@@ -0,0 +1,124 @@
+// -*- Mode: C++ -*-
+//
+// Copyright (C) 2013-2020 Red Hat, Inc.
+//
+// This file is part of the GNU Application Binary Interface Generic
+// Analysis and Instrumentation Library (libabigail).  This library is
+// free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option) any
+// later version.
+
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; see the file COPYING-LGPLV3.  If
+// not, see <http://www.gnu.org/licenses/>.
+//
+// Author: Dodji Seketeli
+
+/// @file
+///
+/// This file contains the declarations of the entry points to
+/// de-serialize an instance of @ref abigail::translation_unit to an
+/// ABI Instrumentation file in libabigail native XML format.
+
+#ifndef __ABG_WRITER_H__
+#define __ABG_WRITER_H__
+
+#include "abg-fwd.h"
+
+namespace abigail
+{
+namespace xml_writer
+{
+
+using namespace abigail::ir;
+
+class write_context;
+
+/// A convenience typedef for a shared pointer to write_context.
+typedef shared_ptr<write_context> write_context_sptr;
+
+write_context_sptr
+create_write_context(const environment *env,
+		     ostream& output_stream);
+
+void
+set_show_locs(write_context& ctxt, bool flag);
+
+void
+set_annotate(write_context& ctxt, bool flag);
+
+void
+set_write_architecture(write_context& ctxt, bool flag);
+
+void
+set_write_corpus_path(write_context& ctxt, bool flag);
+
+void
+set_write_comp_dir(write_context& ctxt, bool flag);
+
+void
+set_short_locs(write_context& ctxt, bool flag);
+
+/// A convenience generic function to set common options (usually used
+/// by Libabigail tools) from a generic options carrying-object, into
+/// a given @ref write_context.
+///
+/// @param ctxt the @ref the write_context to consider.
+///
+/// @param opts the option-carrying object to set the options from.
+/// It must contain data members named: annotate, and show_locs, at
+/// very least.
+template <typename OPTS>
+void
+set_common_options(write_context& ctxt, const OPTS& opts)
+{
+  set_annotate(ctxt, opts.annotate);
+  set_show_locs(ctxt, opts.show_locs);
+  set_write_architecture(ctxt, opts.write_architecture);
+  set_write_corpus_path(ctxt, opts.write_corpus_path);
+  set_write_comp_dir(ctxt, opts.write_comp_dir);
+  set_short_locs(ctxt, opts.short_locs);
+}
+
+void
+set_ostream(write_context& ctxt, ostream& os);
+
+bool
+write_translation_unit(write_context&	       ctxt,
+		       const translation_unit& tu,
+		       const unsigned	       indent);
+
+bool
+write_corpus_to_archive(const corpus& corp,
+			const string& path,
+			const bool  annotate = false);
+
+bool
+write_corpus_to_archive(const corpus& corp,
+			const bool annotate = false);
+
+bool
+write_corpus_to_archive(const corpus_sptr corp,
+			const bool annotate = false);
+
+bool
+write_corpus(write_context&	ctxt,
+	     const corpus_sptr& corpus,
+	     unsigned		indent,
+	     bool		member_of_group = false);
+
+bool
+write_corpus_group(write_context&	    ctx,
+		   const corpus_group_sptr& group,
+		   unsigned		    indent);
+
+}// end namespace xml_writer
+}// end namespace abigail
+
+#endif //  __ABG_WRITER_H__
